b64b10abd1df5e0856366b6114cdd2814d5aec17
Salvador Hilares <salvador.hilares@utec.edu.pe>
2022-08-01 23:05:12 -0500

    First task

--- FT-M1-master/01-IntroToCS/homework/homework.js
+++ FT-M1-master/01-IntroToCS/homework/homework.js
@@ -2,12 +2,20 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  var numero = 0;
+  for(let i=0; i<num.length; i++)
+    numero = numero + Math.pow(2,i) * parseInt(num[num.length-i-1]);
+  return numero;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  var numero = "";
+  while(num!=0){
+    numero = (num%2).toString() + numero;
+    num = Math.floor(num/2);
+  }
+  return numero;
 }
 
 
